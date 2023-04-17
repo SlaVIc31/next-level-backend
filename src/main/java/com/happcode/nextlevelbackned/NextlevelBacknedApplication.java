122ddd9fa0e73340cdc227762bc23494009b5ca0
Karol Kuc <kkuc@happcode.com>
2023-04-17 11:34:27 +0100

    Initial commit

---
+++ src/main/java/com/happcode/nextlevelbackned/NextlevelBacknedApplication.java
@@ -0,0 +1,13 @@
+package com.happcode.nextlevelbackned;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextlevelBacknedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NextlevelBacknedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/happcode/nextlevelbackned/NextlevelBacknedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.happcode.nextlevelbackned;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextlevelBacknedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
